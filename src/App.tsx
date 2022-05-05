e4f6495b34d8451c7868ca01af34feb8b8e0aa07
Rosa, Genesis <mrgenesis@hotmail.com>
2022-05-05 08:02:28 -0400

    Um teste para verificar que as variáveis estão sendo definidas corretamente

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import RootPage from './pages/Root';
 
+// Um teste para verificar que as variáveis estão sendo definidas corretamente.
+console.log('REACT_APP_TEST', process.env.REACT_APP_TEST);
 
 export default function StickyFooter() {
   return (
