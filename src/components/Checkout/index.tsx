c0e37ecc57f637919b666b088ed7c90dcc291097
Rosa, Genesis <mrgenesis@hotmail.com>
2022-05-05 08:06:19 -0400

    Correção na view do checkout

--- src/components/Checkout/index.tsx
+++ src/components/Checkout/index.tsx
@@ -61,7 +61,7 @@ export default function Checkout() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+      <Container component="main" maxWidth="sm" sx={{ mb: 4, mt: 10 }}>
         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
           <Typography component="h1" variant="h4" align="center">
             Coleta de dados
