892e06151df27635460d2ccf2516b62f1f185f08
Rosa, Genesis <mrgenesis@hotmail.com>
2022-05-05 07:54:22 -0400

    modificada a página home

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,15 +1,13 @@
 
 
 import { Routes, Route } from "react-router-dom";
-import Back from "../components/Back";
 import Checkout from "../components/Checkout";
 import NetClaroHomePage from "./NetClaroHomePage";
 
 function RootPage() {
   return (
     <Routes>
-      <Route path="/" element={<Back />} />
-      <Route path="/net-claro" element={<NetClaroHomePage /> } />
+      <Route path="/" element={<NetClaroHomePage />} />
       <Route path="/cadastro" element={<Checkout /> } />
     </Routes>
   );
