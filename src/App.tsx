ceda1cc96dae8cabe2719a03260e889fc25aed2b
Rosa, Genesis <mrgenesis@hotmail.com>
2022-05-09 10:40:32 -0400

    Removido um console de teste

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import RootPage from './pages/Root';
 
-// Um teste para verificar que as variáveis estão sendo definidas corretamente.
-console.log('REACT_APP_TEST', process.env.REACT_APP_TEST);
-
 export default function StickyFooter() {
   return (
     <Box
