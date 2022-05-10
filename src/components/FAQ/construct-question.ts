0c79821911eb88174cd480c4bfbce297636b1f45
Rosa, Genesis <mrgenesis@hotmail.com>
2022-05-09 22:50:25 -0400

    Adicionadas as perguntas frequentes

---
+++ src/components/FAQ/construct-question.ts
@@ -0,0 +1,9 @@
+
+export class ContructQuestion {
+  question: string;
+  answer: string;
+  constructor(question: string, answer: string) {
+    this.question = question;
+    this.answer = answer;
+  }
+}
--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -4,18 +4,13 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { uid } from '../../commun/utils/uid';
+import { questions } from './questions';
 
-const FAQs = [
-  { question: 'pergunta1', answer: ' is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when' },
-  { question: 'pergunta2', answer: ' is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when' },
-  { question: 'pergunta3', answer: ' is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when' },
-  { question: 'pergunta4', answer: ' is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when' },
-];
 export default function FAQ() {
   return (
     <div>
       <Typography component={'h2'} variant='h4'>Perguntas frequentes</Typography>
-      {FAQs.map(item => {
+      {questions.map(item => {
         return (
           <Accordion key={uid()}>
             <AccordionSummary
@@ -23,7 +18,7 @@ export default function FAQ() {
               aria-controls="panel1a-content"
               id="panel1a-header"
             >
-              <Typography>{item.question}</Typography>
+              <Typography variant='h6'>{item.question}</Typography>
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
